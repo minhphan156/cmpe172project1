1c5afc3d99c4fd40b4c3183189ab4085c983cdba
Minh Phan <minhphan156@gmail.com>
2019-10-23 21:50:58 -0700

    heroku prepare deploy

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const config = {
+  user: "cmpe172project1",
+  host: "cmpe172project1.c2l4gt8lgqqd.us-east-2.rds.amazonaws.com",
+  database: "cmpe172project1",
+  password: "cmpe172project1",
+  port: 5432,
+  accessKeyId: "AKIAZKAC5DUBDVBZQ7UP",
+  secretAccessKey: "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",
+  Bucket: "cmpe172project1"
+};
+module.exports = config;
