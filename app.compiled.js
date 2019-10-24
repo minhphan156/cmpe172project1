4c19e9aa9bf0bb39034d8f04b5d4cda8480c0c0d
Minh Phan <minhphan156@gmail.com>
2019-10-23 18:43:07 -0700

    change folder structure again 2

--- app.compiled.js
+++ app.compiled.js
@@ -1,47 +1,49 @@
+"use strict";
+
 var createError = require("http-errors");
+
 var express = require("express");
+
 var path = require("path");
+
 var cookieParser = require("cookie-parser");
+
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
+
 var usersRouter = require("./routes/users");
 
-var app = express();
-// serve static web of react frontend
-app.use(express.static(path.join(__dirname, "/../front_end", "build")));
+var app = express(); // serve static web of react frontend
 
-var cors = require("cors");
+app.use(express["static"](path.join(__dirname, "front_end", "build")));
 
-// view engine setup
+var cors = require("cors"); // view engine setup
+
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cors());
-
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express["static"](path.join(__dirname, "public")));
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/users", usersRouter); // catch 404 and forward to error handler
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
-});
+}); // error handler
 
-// error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {}; // render the error page
 
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
-
 module.exports = app;
